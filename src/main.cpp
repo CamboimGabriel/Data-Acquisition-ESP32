e7d710417cd169ec65aa4d40d4c6af06fbcd72e6
Gabriel Camboim <gabrielcamboim99@gmail.com>
2021-01-06 17:39:17 -0300

    versao final

--- src/main.cpp
+++ src/main.cpp
@@ -10,43 +10,74 @@
 
 TaskHandle_t google;
 
-//solo 1,2,3,4
-DHT solo1(33, DHT22);
-DHT solo2(32, DHT22);
-DHT solo3(26, DHT22);
-DHT solo4(27, DHT22);
+#define user "VIVOFIBRA-C048"
+#define password "722345C048"
+
+DHT solo11(32, DHT22);
+DHT solo12(33, DHT22);
+DHT solo13(25, DHT22);
+DHT solo14(26, DHT22);
+
+//4, 5, 22, 23
+//34 35 n pega ADC
+
+DHT solo21(27, DHT22); 
+DHT solo22(14, DHT22); 
+DHT solo23(12, DHT22);
+DHT solo24(13, DHT22); 
+
+DHT solo31(5, DHT22); 
+DHT solo32(4, DHT22);
+DHT solo33(22, DHT22);
+DHT solo34(23, DHT22);
 
 //ambiente
-DHT ambiente(25, DHT11);
+DHT ambiente(19, DHT11);
 
+//Sensores de temp a prova d'agua
 OneWire oneWire(18);
-
 DallasTemperature sensors(&oneWire);
 
 WiFiClientSecure client;
 
 String textFix = "GET /forms/d/e/1FAIpQLSeMRePPcHbF6prkBmC6bcII-fQSmF6d_Z5HYZuBBG6CjxmJZA/formResponse?ifq&entry.1839580957=";
- 
-float temperaturaENTRADA;
-float temperaturaSAIDA; 
-float temperaturaSOLO1;
-float temperaturaSOLO2;
-float temperaturaSOLO3;
-float temperaturaSOLO4;
-float temperaturaAMBIENTE;
-float vazao;
 
-int contaPulso; //Variável para a quantidade de pulsos
+//sensores de temperatura
+float temperaturaENTRADA = 0;
+float temperaturaSAIDA = 0;
+float temperaturaSOLO11 = 0;
+float temperaturaSOLO12 = 0;
+float temperaturaSOLO13 = 0;
+float temperaturaSOLO14 = 0;
+float temperaturaSOLO21 = 0;
+float temperaturaSOLO22 = 0;
+float temperaturaSOLO23 = 0;
+float temperaturaSOLO24 = 0;
+float temperaturaSOLO31 = 0;
+float temperaturaSOLO32 = 0;
+float temperaturaSOLO33 = 0;
+float temperaturaSOLO34 = 0;
+float temperaturaAMBIENTE = 0;
+
+//Sensor de vazão
+const int PINO_SENSOR = 21;
+unsigned long contador = 0;
+const float FATOR_CALIBRACAO = 4.5;
+float fluxo = 0;
+unsigned long tempo_antes = 0;
+
+void contador_pulso()
+{
+    contador++;
+}
 
 void connectWiFi()
 {
     Serial.println("Connecting");
 
-    //Troque pelo nome e senha da sua rede WiFi
-    WiFi.begin("Pex_Movel2.4", "pexgeral@2020");
-    
-    //Espera enquanto não estiver conectado
-    while(WiFi.status() != WL_CONNECTED)
+    WiFi.begin(user,password);
+
+    while (WiFi.status() != WL_CONNECTED)
     {
         Serial.print(".");
         delay(500);
@@ -55,97 +86,130 @@ void connectWiFi()
     Serial.println();
     Serial.print("Connected to ");
     Serial.println(WiFi.SSID());
-    
 }
 
-void wifiConnectionTask(void* param)
+void wifiConnectionTask(void *param)
 {
-    while(true)
+    while (true)
     {
-        //Se a WiFi não está conectada
-        if(WiFi.status() != WL_CONNECTED)
+        if (WiFi.status() != WL_CONNECTED)
         {
-            //Manda conectar
             connectWiFi();
         }
-
-        //Delay de 100 ticks
         vTaskDelay(100);
     }
 }
 
-void vazaoTask(void* param)
+void googleConnectionTask(void *param)
 {
-    while(true)
+    while (true)
     {
-        contaPulso = 0;//Zera a variável
-        sei(); //Habilita interrupção
-        vTaskDelay (1000); //Aguarda 1 segundo
-        cli(); //Desabilita interrupção
-        vazao = contaPulso / 5.5;
-        printf("Vazão: %f L/min\n", vazao);
-    }
-}
+        Serial.print("Temperatura de Entrada: ");
+        Serial.println(temperaturaENTRADA);
 
-void incpulso ()  {
- contaPulso++; //Incrementa a variável de pulsos
-}
+        Serial.print("Temperatura de Saída: ");
+        Serial.println(temperaturaSAIDA);
 
-void googleConnectionTask(void* param)
-{
-    while(true){
+        Serial.print("Temperatura do Solo 1-1: ");
+        Serial.println(temperaturaSOLO11);
+        Serial.print("Temperatura do Solo 1-2: ");
+        Serial.println(temperaturaSOLO12);
+        Serial.print("Temperatura do Solo 1-3: ");
+        Serial.println(temperaturaSOLO13);
+        Serial.print("Temperatura do Solo 1-4: ");
+        Serial.println(temperaturaSOLO14);
 
-    if (client.connect("docs.google.com", 443) == 1)//Tenta se conectar ao servidor do Google docs na porta 443 (HTTPS)
-    {
+        Serial.print("Temperatura do Solo 2-1: ");
+        Serial.println(temperaturaSOLO21);
+        Serial.print("Temperatura do Solo 2-2: ");
+        Serial.println(temperaturaSOLO22);
+        Serial.print("Temperatura do Solo 2-3: ");
+        Serial.println(temperaturaSOLO23);
+        Serial.print("Temperatura do Solo 2-4: ");
+        Serial.println(temperaturaSOLO24);
 
-        String toSend = textFix;//Atribuimos a String auxiliar na nova String que sera enviada
+        Serial.print("Temperatura do Solo 3-1: ");
+        Serial.println(temperaturaSOLO31);
+        Serial.print("Temperatura do Solo 3-2: ");
+        Serial.println(temperaturaSOLO32);
+        Serial.print("Temperatura do Solo 3-3: ");
+        Serial.println(temperaturaSOLO33);
+        Serial.print("Temperatura do Solo 3-4: ");
+        Serial.println(temperaturaSOLO34);
 
-        //temperatura de entrada
-        toSend += temperaturaENTRADA;
+        Serial.print("Temperatura do Ambiente: ");
+        Serial.println(temperaturaAMBIENTE);
 
-        //temperatura de saída
-        toSend += "&entry.998674289=";
-        toSend += temperaturaSAIDA;
+        Serial.print("Fluxo de água (L/min): ");
+        Serial.println(fluxo);
 
-        //vazao
-        toSend += "&entry.1925389049=";
-        toSend += vazao;
+        Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
-        //temperatura do solo 1
-        toSend += "&entry.863873917=";
-        toSend += temperaturaSOLO1;
+        if (client.connect("docs.google.com", 443) == 1)
+        {
 
-        //temperatura do solo 2
-        toSend += "&entry.1518420806=";
-        toSend += temperaturaSOLO2;
+            String toSend = textFix;
 
-        //temperatura do solo 3
-        toSend += "&entry.317280867=";
-        toSend += temperaturaSOLO3;
+            toSend += temperaturaENTRADA;
 
-        //temperatura do solo 4
-        toSend += "&entry.941940502=";
-        toSend += temperaturaSOLO4;
+            toSend += "&entry.998674289=";
+            toSend += temperaturaSAIDA;
 
-        //temperatura ambiente
-        toSend += "&entry.75671070=";
-        toSend += temperaturaAMBIENTE;
-        
-        toSend += "&submit=Submit HTTP/1.1";//Completamos o metodo GET para nosso formulario.
-        client.println(toSend);//Enviamos o GET ao servidor-
-        client.println("Host: docs.google.com");//-
-        client.println();//-
-        client.stop();//Encerramos a conexao com o servidor
-        Serial.println("Dados enviados.");//Mostra no monitor que foi enviado
-        
-    }
-    else
-    {
-        Serial.println("Erro ao se conectar");//Se nao for possivel conectar no servidor, ira avisar no monitor.
-    }
+            toSend += "&entry.1925389049=";
+            toSend += fluxo;
 
-    vTaskDelay(10000);
+            toSend += "&entry.863873917=";
+            toSend += temperaturaSOLO11;
 
+            toSend += "&entry.1518420806=";
+            toSend += temperaturaSOLO12;
+
+            toSend += "&entry.317280867=";
+            toSend += temperaturaSOLO13;
+
+            toSend += "&entry.941940502=";
+            toSend += temperaturaSOLO14;
+
+            toSend += "&entry.177193093=";
+            toSend += temperaturaSOLO21;
+
+            toSend += "&entry.394934538=";
+            toSend += temperaturaSOLO22;
+
+            toSend += "&entry.1773083222=";
+            toSend += temperaturaSOLO23;
+
+            toSend += "&entry.1711785333=";
+            toSend += temperaturaSOLO24;
+
+            toSend += "&entry.1119390422=";
+            toSend += temperaturaSOLO31;
+
+            toSend += "&entry.525185917=";
+            toSend += temperaturaSOLO32;
+
+            toSend += "&entry.1983348490=";
+            toSend += temperaturaSOLO33;
+
+            toSend += "&entry.1238373872=";
+            toSend += temperaturaSOLO34;
+
+            toSend += "&entry.75671070=";
+            toSend += temperaturaAMBIENTE;
+
+            toSend += "&submit=Submit HTTP/1.1";     //Completamos o metodo GET para nosso formulario.
+            client.println(toSend);                  //Enviamos o GET ao servidor-
+            client.println("Host: docs.google.com"); //-
+            client.println();                        //-
+            client.stop();                           //Encerramos a conexao com o servidor
+            Serial.println("Dados enviados.");       //Mostra no monitor que foi enviado
+        }
+        else
+        {
+            Serial.println("Erro ao se conectar"); //Se nao for possivel conectar no servidor, ira avisar no monitor.
+        }
+
+        vTaskDelay(10000);
     }
 }
 
@@ -153,44 +217,45 @@ void setup()
 {
     Serial.begin(115200);
 
-    solo1.begin();
-    solo2.begin();
-    solo3.begin();
-    solo4.begin();
+    solo11.begin();
+    solo12.begin();
+    solo13.begin();
+    solo14.begin();
+
+    solo21.begin();
+    solo22.begin();
+    solo23.begin();
+    solo24.begin();
+
+    solo31.begin();
+    solo32.begin();
+    solo33.begin();
+    solo34.begin();
+
+    //sensor de agua
+    pinMode(PINO_SENSOR, INPUT_PULLUP);
+
     ambiente.begin();
     sensors.begin();
 
     connectWiFi();
 
-    pinMode(2, INPUT);
-    attachInterrupt(0, incpulso, RISING);
-
-    //Cria uma nova tarefa no core 0
     xTaskCreatePinnedToCore(
-        wifiConnectionTask,     //Função que será executada
-        "wifiConnectionTask",   //Nome da tarefa
-        10000,                  //Tamanho da memória disponível (em WORDs)
-        NULL,                   //Não vamos passar nenhum parametro
-        2,                      //prioridade
-        NULL,                   //Não precisamos de referência para a tarefa
-        0);                     //Número do core
-    
-  xTaskCreatePinnedToCore(
+        wifiConnectionTask, 
+        "wifiConnectionTask", 
+        10000,             
+        NULL,             
+        2,                  
+        NULL,                
+        0);        
+
+    xTaskCreatePinnedToCore(
         googleConnectionTask,
-        "googleConnectionTask", 
+        "googleConnectionTask",
         10000,
         NULL,
         2,
         &google,
-        1);
-   
-   xTaskCreatePinnedToCore(
-        vazaoTask,
-        "vazaoTask", 
-        10000,
-        NULL,
-        2,
-        NULL,
         1);
 }
 
@@ -198,17 +263,40 @@ void loop()
 {
     vTaskSuspend(google);
 
+    if ((millis() - tempo_antes) > 1000)
+    {
+        detachInterrupt(PINO_SENSOR);
+
+        fluxo = ((1000.0 / (millis() - tempo_antes)) * contador) / FATOR_CALIBRACAO;
+
+        contador = 0;
+
+        tempo_antes = millis();
+
+        attachInterrupt(PINO_SENSOR, contador_pulso, FALLING);
+    }
+
     sensors.requestTemperatures();
 
     temperaturaENTRADA = sensors.getTempCByIndex(0);
     temperaturaSAIDA = sensors.getTempCByIndex(1);
 
-    temperaturaSOLO1 = solo1.readTemperature();
-    temperaturaSOLO2 = solo2.readTemperature();
-    temperaturaSOLO3 = solo3.readTemperature();
-    temperaturaSOLO4 = solo4.readTemperature();
+    temperaturaSOLO11 = solo11.readTemperature();
+    temperaturaSOLO12 = solo12.readTemperature();
+    temperaturaSOLO13 = solo13.readTemperature();
+    temperaturaSOLO14 = solo14.readTemperature();
+
+    temperaturaSOLO21 = solo21.readTemperature();
+    temperaturaSOLO22 = solo22.readTemperature();
+    temperaturaSOLO23 = solo23.readTemperature();
+    temperaturaSOLO24 = solo24.readTemperature();
+
+    temperaturaSOLO31 = solo31.readTemperature();
+    temperaturaSOLO32 = solo32.readTemperature();
+    temperaturaSOLO33 = solo33.readTemperature();
+    temperaturaSOLO34 = solo34.readTemperature();
+
     temperaturaAMBIENTE = ambiente.readTemperature();
 
     vTaskResume(google);
 }
-
